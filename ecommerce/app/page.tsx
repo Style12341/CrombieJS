340d481713bcb712be0be7f1655633ffe592db59
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-03-10 16:25:12 -0300

    feat: partial landing page

--- ecommerce/app/page.tsx
+++ ecommerce/app/page.tsx
@@ -1,101 +1,79 @@
 import Image from "next/image";
+import { ReactNode } from "react";
+import LandingImage from "../public/images/landing_image.jpg";
+import Logo from "../public/images/ecommerce_logo.svg";
+import Shirts from "../public/images/shirts_landing.jpeg";
+import Products from "../public/images/products_landing.jpg";
 
-export default function Home() {
+const Section = ({ children, className = "" }: { children: ReactNode, className: string }) => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <section className={`relative w-full h-screen flex items-center justify-center ${className}`}>
+      {children}
+    </section>
+  );
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function LandingPage() {
+  return (
+    <div className="w-full h-auto">
+      <Section className="relative bg-blue-500 text-white">
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src={LandingImage}
+            alt="Landing image"
+            layout="fill"
+            objectFit="cover"
+            className="z-0"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="absolute top-4 left-4 z-10" id="logo">
+          <Image src={Logo} alt="Logo" height={100} width={100} />
+        </div >
+        <div className="flex flex-col items-center z-10 gap-10">
+          <h1 className="relative bottom-56 text-white lg:text-8xl sm:text-6xl text-4xl">
+            Eccomerce
+          </h1>
+          <h2 className="relative top-56 text-slate-300 lg:text-4xl sm:text-3xl text-2xl">
+            Your one stop shop for everything
+          </h2>
+        </div>
+      </Section>
+      <Section className="bg-black gap-8 flex-wrap">
+        <Image src={Shirts} alt="Pile of shirts" className="rounded-md flex-1 max-w-screen-sm  mx-6" />
+        <div className="flex flex-col items-center text-white flex-1 min-w-60  max-w-2xl">
+          <h2 className="text-4xl">Shop the latest trends</h2>
+          <p className="mx-6"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque at nesciunt esse voluptatem eligendi! Totam similique sequi odit accusamus voluptate autem ullam. Quod asperiores animi error sequi perferendis. Voluptatum, fugit!</p>
+        </div>
+      </Section>
+      <Section className="bg-black gap-8 flex-wrap">
+        <div className="flex flex-col items-center">
+          <h2 className="text-4xl">Our products</h2>
+          <Image src={Products} alt="Accesories" className="rounded-md flex-1 max-w-screen-sm  mx-6" />
+        </div>
+        <ul className="flex flex-col gap-4 text-white flex-1 min-w-60   max-w-2xl">
+          <li className="text-2xl flex gap-2 ">
+            <h2 className="text-6xl text-slate-400">01</h2>
+            <div className="flex flex-col gap-2">
+              <h4 className="text-6xl">Watches</h4>
+              <p>Best watches</p>
+            </div>
+          </li>
+          <li className="text-2xl flex gap-2">
+            <h2 className="text-6xl text-slate-400">02</h2>
+            <div className="flex flex-col gap-2">
+              <h4 className="text-6xl">Shirts</h4>
+              <p>Best shirts</p>
+            </div>
+          </li>
+          <li className="text-2xl flex gap-2">
+            <h2 className="text-6xl text-slate-400">03</h2>
+            <div className="flex flex-col gap-2">
+              <h4 className="text-6xl">Belts</h4>
+              <p>Best Belts</p>
+            </div>
+          </li>
+        </ul>
+      </Section>
     </div>
   );
 }
