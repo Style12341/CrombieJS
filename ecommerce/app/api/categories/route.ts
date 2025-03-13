69325cfcfc861fde89c190b6267c34ce9a1075ae
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-03-13 03:04:09 -0300

    feat: protect post routes for admin

--- ecommerce/app/api/categories/route.ts
+++ ecommerce/app/api/categories/route.ts
@@ -1,4 +1,5 @@
 import getCategories, { createCategory } from "@/app/lib/categories";
+import { checkRole } from "@/utils/roles";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(): Promise<Response> {
@@ -6,7 +7,11 @@ export async function GET(): Promise<Response> {
   return NextResponse.json(categories);
 }
 export async function POST(request: NextRequest): Promise<Response> {
-    const body: Category = await request.json();
-    const cat = await createCategory(body.name);
-    return NextResponse.json(cat, { status: 201 });
+  const isAdmin = await checkRole('ADMIN');
+  if (!isAdmin) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const body: Category = await request.json();
+  const cat = await createCategory(body.name);
+  return NextResponse.json(cat, { status: 201 });
 }
